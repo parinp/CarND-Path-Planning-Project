81756628f3ee946e9ae6aafb408ff3d8567b505b
Parin Pipake <parinpipake@gmail.com>
2020-08-20 00:11:36 +0700

    Updated main code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
+#include "cost.h"
 
 // for convenience
 using nlohmann::json;
@@ -49,14 +51,29 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+  
+  int lane = 1;
+  double vel_ref = 0.0;
+  double vel_limit = 49.5;
+  double acc_limit = 0.224;
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  Vehicle car = Vehicle();
+  car.lane = lane;
+  car.v = vel_ref;
+  car.a = acc_limit;
+  car.state = "ACC";
+  car.target_lane = lane;
+
+  h.onMessage([&vel_ref, &vel_limit, &acc_limit,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy, &car]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+
+    // setting intial and default variables
+
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -90,6 +107,9 @@ int main() {
 
           json msgJson;
 
+          vector<double> ptsx;
+          vector<double> ptsy;
+
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
@@ -98,6 +118,138 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+           //Prediction of best possible waypoint
+
+          int prev_size = previous_path_x.size();
+
+          //detecting cars close by
+
+          if(prev_size>0)
+          {
+            car_s = end_path_s;
+            car.s = end_path_s;
+          }
+          /*
+          for(int i = 0;i<sensor_fusion.size();i++)
+          {
+
+            double check_car_s = sensor_fusion[i][5];
+            double distance = check_car_s - car.s;
+
+            std::cout << distance;
+            std::cout << "\n";
+          }
+          */
+
+          car.possible_states = car.transitions[car.state];
+          plan(car,sensor_fusion,prev_size);
+          change_param(car,sensor_fusion,car_d);
+
+          //std::cout << "\n";
+          //std::cout << car.state;
+          //std::cout << "\n";
+
+          //creating referrence states for x and y
+          //For initializing from the previous paths end point
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if(prev_size < 2 ){
+
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }else{
+
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+
+            double ref_x_prev = previous_path_x[prev_size - 2]; 
+            double ref_y_prev = previous_path_y[prev_size - 2];
+            ref_yaw = atan2(ref_y-ref_y_prev,ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+
+          }
+
+          vector<double> wp1 = getXY(car_s+30,2+4*car.target_lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> wp2 = getXY(car_s+45,2+4*car.target_lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> wp3 = getXY(car_s+60,2+4*car.target_lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> wp4 = getXY(car_s+75,2+4*car.target_lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> wp5 = getXY(car_s+90,2+4*car.target_lane,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          
+          ptsx.push_back(wp1[0]);
+          ptsx.push_back(wp2[0]);
+          ptsx.push_back(wp3[0]);
+          ptsx.push_back(wp4[0]);
+          ptsx.push_back(wp5[0]);
+
+          ptsy.push_back(wp1[1]);
+          ptsy.push_back(wp2[1]);
+          ptsy.push_back(wp3[1]);
+          ptsy.push_back(wp4[1]);
+          ptsy.push_back(wp5[1]);
+
+          for(int i =0 ; i <ptsx.size();i++){
+
+            //shifting x, y to origin and shifting yaw angle to be 0 degrees
+            double shift_x = ptsx[i] - ref_x ; 
+            double shift_y = ptsy[i] - ref_y ; 
+
+            ptsx[i] = shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw);
+            ptsy[i] = shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw);
+
+          }
+
+          tk::spline s;
+
+          s.set_points(ptsx,ptsy);
+
+          // start with all previous points  
+          for(int i = 0; i<previous_path_x.size();i++){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          
+          //calculate how to break up splice for our caclculation
+          double target_x =30;
+          double target_y = s(target_x);
+          double distance = sqrt(pow(target_x,2)+pow(target_y,2));
+
+          double x_add_on = 0;
+
+          for (int i = 1;i<=50-previous_path_x.size();i++)
+          {
+            double N = (distance/(0.02*car.v/2.24));
+            double x_point = x_add_on + target_x/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;   
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = (x_ref *cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref *sin(ref_yaw) + y_ref*cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
